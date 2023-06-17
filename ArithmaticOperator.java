1cb4c7c832d953c6e92563e561d1a2cfffcb7e54
Devansh <Devansh@LAPTOP-JU354M2T>
2023-06-17 23:08:19 +0530

    Write a Java program that reads a number and display the square, cube, and fourth power

---
+++ ArithmaticOperator.java
@@ -0,0 +1,12 @@
+package firstprogram;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		int i=2;
+		System.out.println("Squre: " + (i*i));
+		System.out.println("cube: " +(i*i*i));
+		System.out.println("fourth power: "+(i*i*i*i) );
+	}
+
+}
